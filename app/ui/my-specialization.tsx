25175e55d0a591cb7a4f4b026352025803929169
Santosh JS <santoshjsutar@gmail.com>
2025-07-11 03:05:27 +0530

    home page upgrades

---
+++ app/ui/my-specialization.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function MySpecialization() {
+  return (
+    <div className="text-[25.14px] text-[#D9D9D9] leading-[1.3]">
+      I specialize in a multidisciplinary approach to brand identity, UI/
+      <br />
+      UX, packaging, and 3D design. Through strategic research and user-centric
+      principles, I create visually compelling, functional solutions that
+      enhance experiences and drive engagement.
+    </div>
+  );
+}
--- app/ui/work-card.tsx
+++ app/ui/work-card.tsx
@@ -1,6 +1,8 @@
-import { Span } from "next/dist/trace";
+import localFont from "next/font/local";
 import Image from "next/image";
 import React, { Fragment } from "react";
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
 
 type WorkLabelType = {
   image: string;
@@ -9,6 +11,9 @@ type WorkLabelType = {
   skills: string[];
   legendColor: string;
 };
+const neulisSansLight = localFont({
+  src: "../../public/fonts/NeulisSans/Neulis_Sans_Light.otf",
+});
 export default function WorkCard({
   image,
   label,
@@ -31,23 +36,32 @@ export default function WorkCard({
       <label htmlFor={label} className="font-bold text-[31.82px] capitalize">
         {label}
       </label>
-      <p className="text-[23.35px] w-[574px]">{description}</p>
+      <p className={`${neulisSansLight.className} text-[23.35px] w-[574px]`}>
+        {description}
+      </p>
       <div className="flex gap-[9px]">
-        {skills.map((skill, id) => (
-          <Fragment key={`SKILL_${skill}_${id}`}>
-            {id !== 0 && (
-              <Image
-                src={"/assets/common/outline-dot.svg"}
-                alt="outline-dot image"
-                width={11}
-                height={11}
-              />
-            )}
-            <span className={id === 0 ? `text-[${legendColor}]` : ""}>
-              {skill}
-            </span>
-          </Fragment>
-        ))}
+        {skills.map((skill, id) => {
+          const conditionalClasses = {
+            "text-[#FF57E0]": id === 0 && label === "ayuran",
+            "text-[#2DE6B4]": id === 0 && label === "playdale",
+            "text-[#9C85FF]": id === 0 && label === "quanwave",
+          };
+          const baseClasses = neulisSansLight.className;
+          const mergedClasses = twMerge(clsx(baseClasses, conditionalClasses));
+          return (
+            <Fragment key={`SKILL_${skill}_${id}`}>
+              {id !== 0 && (
+                <Image
+                  src={"/assets/common/outline-dot.svg"}
+                  alt="outline-dot image"
+                  width={11}
+                  height={11}
+                />
+              )}
+              <span className={mergedClasses}>{skill}</span>
+            </Fragment>
+          );
+        })}
       </div>
     </div>
   );
